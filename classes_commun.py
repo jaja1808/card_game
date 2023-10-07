a7783bbaf9611d31cc2a52101085ee0a4d9f4ba8
Prince <rushayigiprince2502@gmail.com>
2023-10-07 14:00:09 +0200

    Setting up Schematics

---
+++ classes_commun.py
@@ -0,0 +1,52 @@
+import numpy as np
+import random as rd
+
+
+# definition de la classe carte
+class carte:
+    def __init__(self,nom_carte,couleur):
+        self.__nom_carte=nom_carte
+        self.__couleur=couleur
+
+        # j'ajoute une test
+
+# 
+    def get__nom_carte(self):
+        return self.__nom_carte
+    
+    def get__couleur(self):
+        return self.__couleur
+   
+    def set__nom_carte(self,nom_carte):
+        self.__nom_carte=nom_carte
+    
+    def set__couleur(self,couleur):
+        self.__couleur=couleur
+
+
+# définition de la classe carte  attouf
+class carte_atout(carte):
+    def __init__(self, nom_carte, couleur,score):
+        super().__init__(nom_carte, couleur)
+        self.__score=score
+
+    def get__score(self):
+        return self.__score
+    
+    def set__score(self,score):
+        self.__score=score
+
+
+# définition de la classe  non attouf(normal)
+class carte_normal(carte):
+    def __init__(self, nom_carte, couleur,score):
+        super().__init__(nom_carte, couleur)
+        self.__score=score
+
+    def get__score(self):
+        return self.__score
+    
+    def set__score(self,score):
+        self.__score=score
+
+
