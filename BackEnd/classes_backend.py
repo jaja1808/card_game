3b3a02aa872342fe3aeeda92b962be85f1653b34
alainlattouf97 <alainlattouf97@gmail.com>
2023-10-18 17:32:43 +0200

    alain commit

--- BackEnd/classes_backend.py
+++ BackEnd/classes_backend.py
@@ -2,7 +2,6 @@ import numpy as np
 import random as rd
 
 import classes_commun
-<<<<<<< HEAD
 
 
 # papi =joueur()
@@ -10,6 +9,9 @@ import classes_commun
 # prince =joueur()
 # alain =joueur()
 # nouveau_jeu=jeu (papi,prince,alain,brian)
-=======
 from classes_commun import *
->>>>>>> e892976cf375bd5d42d95f8777eaedc8bbc2be0b
+
+
+alain =Player
+
+
