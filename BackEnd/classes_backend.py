c64ad603984d2dec9091c5700cfa1bb153806392
alainlattouf97 <alainlattouf97@gmail.com>
2023-11-11 16:50:59 +0100

    nettoyage fichier

--- BackEnd/classes_backend.py
+++ BackEnd/classes_backend.py
@@ -1,13 +1,3 @@
 import numpy as np
 import random as rd
-
-import classes_commun
-
-
-# papi =joueur()
-# brian =joueur()
-# prince =joueur()
-# alain =joueur()
-# nouveau_jeu=jeu (papi,prince,alain,brian)
 from classes_commun import *
-
